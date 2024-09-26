f0444f0cd1a8437b802d4c0879e348f22f9c5c63
ahmedmahbab <ahmed.mahbab@gmail.com>
2024-09-26 12:47:00 +0100

    Update Accueil.py

--- pages/Accueil.py
+++ pages/Accueil.py
@@ -1,7 +1,7 @@
 def accueil_page():
-    # حذف أو تعليق الشيفرة المتعلقة بـ CSS
-    
-    # تجربة تحميل صورة
-    st.image("assets/logo.jpg", use_column_width=False, width=200, caption="شعار الشركة")
+    try:
+        st.image("assets/logo.jpg", use_column_width=False, width=200, caption="شعار الشركة")
+    except Exception as e:
+        st.write(f"حدث خطأ أثناء تحميل الصورة: {e}")
     
     st.write("مرحبًا بكم في تطبيق إدارة الحملات")
