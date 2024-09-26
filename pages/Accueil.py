cb7b2c911b233272e8afe0d3e86b12ec7e29f4a7
ahmedmahbab <ahmed.mahbab@gmail.com>
2024-09-26 12:34:34 +0100

    Update Accueil.py

--- pages/Accueil.py
+++ pages/Accueil.py
@@ -18,8 +18,8 @@ def accueil_page():
         """, unsafe_allow_html=True
     )
 
-    # استخدام رابط مباشر للصورة بدلاً من تحميلها محليًا
-    st.image("https://lh6.googleusercontent.com/uUBr22ZS3x71Gr_HslpAWK8jRN4LqJ-sA1vb2YOlrw8w89M0UUdzX9eI8QDc9RgDru6wC225QkKHONrynX_7V3hd911H05bJ4nf5W-d9IixSOvs_Msjq48lOv22wEkNI1Q=w1280", use_column_width=False, width=200, caption="شعار الشركة")
+    # تأكد من المسار الصحيح للملف (هنا أفترض وجود الصورة في مجلد static)
+    st.image("logo.png", use_column_width=False, width=200, caption="شعار الشركة")
     st.markdown('<p class="main-title">مرحبًا بكم في تطبيق إدارة الحملات</p>', unsafe_allow_html=True)
     st.markdown('<p class="sub-title">إدارة الحملات بسهولة وفعالية</p>', unsafe_allow_html=True)
 
